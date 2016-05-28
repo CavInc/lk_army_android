8cbb02c233eb4e541d367bfd9411678d9d41ef18
CavInc <cavinc20@gmail.com>
2016-05-28 15:49:03 +0700

    Исправлено неверноре разрешение конфликта

--- app/src/main/java/cav/lkarmy/FullscreenActivity.java
+++ app/src/main/java/cav/lkarmy/FullscreenActivity.java
@@ -1,5 +1,6 @@
 package cav.lkarmy;
 
+import cav.lkarmy.util.MyWebChromeClient;
 import cav.lkarmy.util.MyWebViewClient;
 import cav.lkarmy.util.SystemUiHider;
 
@@ -8,6 +9,8 @@ import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebView;
@@ -48,19 +51,33 @@ public class FullscreenActivity extends Activity {
      */
     private SystemUiHider mSystemUiHider;
 
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_fullscreen);
 
-        final View controlsView = findViewById(R.id.fullscreen_content_controls);
+        //final View controlsView = findViewById(R.id.fullscreen_content_controls);
         //final View contentView = findViewById(R.id.fullscreen_content);
-        final WebView webView = (WebView) findViewById(R.id.webView);
+        webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setSupportZoom(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            webView.getSettings().setDisplayZoomControls(false);
+        }
+        webView.getSettings().setBuiltInZoomControls(true);
+
+        webView.getSettings().setLoadWithOverviewMode(true); // ? маштабирует сайт по размеру экрана
+        webView.getSettings().setUseWideViewPort(true);  // ?  маштабирует сайт по размеру экрана
+
+        webView.setVerticalScrollBarEnabled(true);
+        webView.setHorizontalScrollBarEnabled(true);
+
         webView.setWebViewClient(new MyWebViewClient());
+        webView.setWebChromeClient(new MyWebChromeClient());
        // webView.loadUrl("http://developer.alexanderklimov.ru/android");
         webView.loadUrl("http://kempir.com:8000");
         String uname="odminko";
@@ -177,5 +194,32 @@ public class FullscreenActivity extends Activity {
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // обработчик меню
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.about) {
+            return true;
+        }
+        if (id==R.id.setting) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

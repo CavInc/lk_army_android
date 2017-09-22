762765a7aeaec79f9eb425cee5b192eb8114159d
CavInc <cavinc20@gmail.com>
2017-09-22 16:44:50 +0700

    Шаблоны и филтры

--- app/src/main/java/cav/lkarmy/Preferences.java
+++ app/src/main/java/cav/lkarmy/Preferences.java
@@ -5,8 +5,17 @@ import android.preference.PreferenceActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import ru.tinkoff.decoro.MaskImpl;
+import ru.tinkoff.decoro.parser.UnderscoreDigitSlotsParser;
+import ru.tinkoff.decoro.slots.Slot;
+import ru.tinkoff.decoro.watchers.FormatWatcher;
+import ru.tinkoff.decoro.watchers.MaskFormatWatcher;
+
+//https://habrahabr.ru/company/tinkoff/blog/312968/
+//http://www.vr-online.ru/content/pishem-igru-dlja-android-chast-5-khranenie-nastroek-3974
 public class Preferences extends PreferenceActivity {
 
     @Override
@@ -15,7 +24,20 @@ public class Preferences extends PreferenceActivity {
         addPreferencesFromResource(R.xml.preferences);
         EditTextPreference snils = (EditTextPreference) findPreference("snils");
 
+        Slot[] slots = new UnderscoreDigitSlotsParser().parseSlots("___-___-___ __");
+        FormatWatcher formatWatcher = new MaskFormatWatcher( // форматировать текст будет вот он
+                MaskImpl.createTerminated(slots)
+        );
+        formatWatcher.installOn(snils.getEditText()); // устанавливаем форматтер на любой TextView
+/*
+        EditTextPreference user_num = (EditTextPreference) findPreference("imSign");
+        EditText imSign = user_num.getEditText();
+        Slot[] slots2 = new UnderscoreDigitSlotsParser().parseSlots("__-______");
+        FormatWatcher formatWatcher2 = new MaskFormatWatcher(MaskImpl.createNonTerminated(slots2));
+        formatWatcher2.installOn(imSign);
+        */
     }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -29,4 +51,6 @@ public class Preferences extends PreferenceActivity {
         Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
         root.addView(bar, 0); // insert at top
     }
+
+
 }
---
+++ app/src/main/java/cav/lkarmy/util/SnilsWatcher.java
@@ -0,0 +1,23 @@
+package cav.lkarmy.util;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+public class SnilsWatcher implements TextWatcher{
+    String mResult = "";
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+}
